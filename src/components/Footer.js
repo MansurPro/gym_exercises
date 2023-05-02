a55f7907ed2a712442675c8aeebceba31021bfc9
Mansurbek Satarov <mansurbek1203@gmail.com>
2023-05-02 11:39:24 -0500

    Footer implemented

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+
+import Logo from '../assets/images/Logo-1.png';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <Box 
+      mt="80px"
+      bgcolor="#fff3f4"
+    >
+      <Stack gap="40px" alignItems="center" px="40px" pt="24px">
+        <img src={Logo} alt='logo' width="200px" height="40px" />
+        <Typography variant='h5' pb="40px" mt="20px">
+          Made with ❤️ by MansurPro
+        </Typography>
+      </Stack>
+    </Box>
   )
 }
 
-export default Footer
+export default Footer;
